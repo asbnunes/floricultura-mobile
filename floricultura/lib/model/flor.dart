3e6713af0aa44830503637ff49c751502a064d28
Andre Bucar <asbnbm00@gmail.com>
2023-06-12 15:46:48 -0300

    Classe Flor
    - Adicionando model para classe flor.

---
+++ floricultura/lib/model/flor.dart
@@ -0,0 +1,13 @@
+class Flor {
+  final String nome;
+  final String cor;
+  final String preco;
+  final String imagem;
+
+  Flor({
+    required this.nome,
+    required this.cor,
+    required this.preco,
+    required this.imagem,
+  });
+}
