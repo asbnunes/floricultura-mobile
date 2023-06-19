9e98d476e17cf96e53bc994e4d3ed0730cd1ddfa
Andre Bucar <asbnbm00@gmail.com>
2023-06-18 23:39:21 -0300

    Animando bottom_nav_bar
    - Adicionando uma animação simples à bottom_nav_bar

--- floricultura/lib/widgets/bottom_nav_bar.dart
+++ floricultura/lib/widgets/bottom_nav_bar.dart
@@ -12,26 +12,44 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-
   int _currentIndex = 0;
+  PageController _pageController = PageController(initialPage: 0);
+
   final List<Widget> _screens = const [
     Home(),
     Carrinho(),
     InfoUsuario(),
   ];
 
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   void _onTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+      _pageController.animateToPage(
+        index,
+        duration: Duration(milliseconds: 200),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  void _onPageChanged(int index) {
     setState(() {
       _currentIndex = index;
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
         children: _screens,
       ),
       bottomNavigationBar: GNav(
@@ -43,9 +61,18 @@ class _BottomNavBarState extends State<BottomNavBar> {
         selectedIndex: _currentIndex,
         onTabChange: _onTapped,
         tabs: const [
-          GButton(icon: Icons.home, text: 'Home',),
-          GButton(icon: Icons.shopping_cart, text: 'Carrinho',),
-          GButton(icon: Icons.account_circle,text: 'Minha Conta',),
+          GButton(
+            icon: Icons.home,
+            text: 'Home',
+          ),
+          GButton(
+            icon: Icons.shopping_cart,
+            text: 'Carrinho',
+          ),
+          GButton(
+            icon: Icons.account_circle,
+            text: 'Minha Conta',
+          ),
         ],
       ),
     );
